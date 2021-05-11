a9044bd108fe44ccb19cd991c3f4267b15c94c9a
Aimee <aimeedu16@gmail.com>
2021-05-11 14:19:49 -0400

    p8 done

--- 8. Corner Detection/main.cpp
+++ 8. Corner Detection/main.cpp
@@ -20,12 +20,17 @@ class Image{
     }
 
     // methods
+
     void read_header(ifstream &input){
         input >> numRows >> numCols >> minVal >> maxVal >> label;
     }
         
     void write_header(ofstream &w){
         w << numRows<< " " << numCols<< " " << minVal << " " << maxVal << endl;
+    }
+        
+    void write_label(ofstream &w){
+        w << label << endl;
     }
 
     // take cares of zeroFrame. p = 2, extra columns or rows
@@ -94,8 +99,8 @@ class kCurvature{
     vector<boundaryPt> PtAry;
 
     public:
-    kCurvature(ifstream &input){
-        K = 5;            
+    kCurvature(ifstream &input, int k){
+        K = k;            
         init(input);
         countPts(input);
     }
@@ -125,28 +130,27 @@ class kCurvature{
     }
 
     void cornerDetection(ofstream &w3){
-        w3 << "debugging-------------------"<< endl;
-        Q = 0;
-        P = K;
-        R = 2*K;
-        int index;
-        // w3 << "ok1: " << Q << " & "<< P << " & "<< R << " & " <<  "index: " << index << endl;
-        while(P != K-1){
-            index = P;
-            int curvature = computeCurvature(Q, P, R, w3);
-            PtAry[index].curvature = curvature;
-            // for debugging
-            w3 << "QPR : " << Q << " - "<< P << " - "<< R << " - " <<  "index: " << index << endl;
-            w3 << "X Y Cur: "<< PtAry[index].x << " " << PtAry[index].y << " " << PtAry[index].curvature << endl;
+        w3 << "Debugging-------------------------------"<< endl;
+        R = -K;
+        P = 0;
+        Q = K;
+        int index = P;
+        for (int i =0; i<PtAry.size(); i++){
+            PtAry[index].curvature = computeCurvature(Q, P, R, w3);
+            w3 << "index: " << index << ", row: "<< PtAry[index].x << ", col: " << PtAry[index].y << ", curvature: " << PtAry[index].curvature << endl;
+            w3 << "index: " << index << ", Q: " << Q << ", P: "<< P << ", R: "<< R << endl;
+            
             P = (P+1)%numsPts;
             Q = (Q+1)%numsPts;
             R = (R+1)%numsPts;
+            index++;
         }
-
     }
 
     double computeCurvature(int q, int p, int r, ofstream &w3){
-        //  is p, q, r the index of the PtAry?
+        if (r<0) {
+            r = r+PtAry.size();
+        }
         int r1 = PtAry[q].x;
         int c1 = PtAry[q].y;
 
@@ -156,40 +160,26 @@ class kCurvature{
         int r3 = PtAry[r].x;
         int c3 = PtAry[r].y;
 
-        w3 << "r1: " << r1 << " c1: " << c1 << " r2: " << r2 << " c2: " << c2 <<" r3: "<< r3 <<" c3: " << c3 << endl; 
+        // w3 << "r1: " << r1 << ", c1: " << c1 << ", r2: " << r2 << ", c2: " << c2 <<", r3: "<< r3 <<", c3: " << c3 << endl; 
 
         double d1 = static_cast<double>(r1-r2);
         if (d1 == 0.0){
-            return 0.0;
+            d1 = 0.5;
         }
         double m1 = static_cast<double>(c1-c2) / d1;
 
         double d2 = static_cast<double>(r2-r3);
         if (d2 == 0.0){
-            return 0.0;
+            d2 = 0.5;
         }
 
         double m2 = static_cast<double>(c2-c3) / d2;
-        w3 << m1 << " - " << m2 << " = " << abs(m1-m2) << endl;
+        // w3 << m1 << " - " << m2 << " = " << abs(m1-m2) << endl;
         return abs(m1-m2);
     }
 
     void computeLocalMaxima(){
         // loop through the PtAry
-        for (int i=2; i<PtAry.size()-2; i++){
-            double c = PtAry[i].curvature; 
-            int n1, n2, n3, n4;
-            n1 = PtAry[i-1].curvature;
-            n2 = PtAry[i-2].curvature;
-            n3 = PtAry[i+1].curvature;
-            n4 = PtAry[i+2].curvature;
-            
-            if (c > n1 && c > n3 && c > n3 && c > n4){
-                PtAry[i].localMax = 1;
-            }else{
-                PtAry[i].localMax = 0;
-            }
-        }
         for (int i=0; i<PtAry.size(); i++){
             double c = PtAry[i].curvature; 
             int n1, n2, n3, n4;
@@ -213,7 +203,7 @@ class kCurvature{
             }else{
                 n4 = PtAry[i+2].curvature;
             }
-            if (c > n1 && c > n3 && c > n3 && c > n4){
+            if (c > n1 && c > n2 && c > n3 && c > n4){
                 PtAry[i].localMax = 1;
             }else{
                 PtAry[i].localMax = 0;
@@ -238,17 +228,10 @@ class kCurvature{
             w1 << PtAry[i].x << " ";
             w1 << PtAry[i].y << " ";
             w1 << PtAry[i].corner << " ";
-            w1 << PtAry[i].curvature << " ";
+            // w1 << PtAry[i].curvature << " ";
             w1 << endl;
         }
     }
-
-    // void display(){
-    //     // plot PtAry to imagAry
-    //     for (int i=0; i<PtAry.size(); i++){
-    //         img.imgAry[PtAry[i].x][PtAry[i].y] = PtAry[i].corner;    
-    //     }
-    // }
 
     void printPtAry(ofstream &w3){
         printBoundary(w3);
@@ -270,51 +253,54 @@ int main(int argc, const char* argv[]){
     ofstream output3;
     output3.open(argv[4]);
 
+    int k;
+    if (argv[5] != NULL){
+        k = atoi(argv[5]);
+    }else{
+        k=1;
+    }
+
     if (input.is_open() && output1.is_open() && output2.is_open() && output3.is_open()){
         // step 1   
         Image* img = new Image(input); 
-
         img->plotPt2Img(input);
-        img->reformatPrettyPrint(output2);
+        // img->reformatPrettyPrint(output2);
         img->write_header(output1);
+        img->write_label(output1);
         img->write_header(output3);
         
         input.close();
         input.open(argv[1]);
-        kCurvature* kcv = new kCurvature(input); 
-        kcv->printPtAry(output3);
+        kCurvature* kcv = new kCurvature(input, k); 
+        // kcv->printPtAry(output3);
         
         // step 2
         kcv->cornerDetection(output3);
-        kcv->printPtAry(output3);
+        // kcv->printPtAry(output3);
 
-        // // // step 3
+        // step 3
         kcv->computeLocalMaxima();
         
-        // // // step 4
+        // step 4
         kcv->markCorner();
 
-        // // // step 5
+        // step 5
         kcv->printBoundary(output1);
         
-        // // step 6, after mark corner
+        // step 6, after mark corner, write 9 to image
         for (int i=0; i<kcv->PtAry.size(); i++){
             img->imgAry[kcv->PtAry[i].x][kcv->PtAry[i].y] = kcv->PtAry[i].corner;
         }
-        
 
-        // // img->plotPt2Img();
-        // kcv->display();
-        // // step 7
+        // step 7
         img->reformatPrettyPrint(output2);
         img->free_heap();
     }else {
         cout << "Error: input or output file is not open!"<< endl;
-
         cout << input.is_open() << endl << output1.is_open() << endl << output2.is_open() << output3.is_open() << endl;
     }
   
-    // step 14
+    // step 8
     input.close();
     output1.close();
     output2.close();
